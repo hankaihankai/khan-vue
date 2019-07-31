be75d1136a36b2f6163530ca23df69deb86247b6
hankai <1245906870@qq.com>
2019-07-31 21:16:55 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,30 +1,51 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+
+import '@/styles/index.scss' // global css
+
 import App from './App'
+import store from './store'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import VueResource from 'vue-resource'
-import axios from 'axios'
 
+import './icons' // icon
+import './permission' // permission control
+import './utils/error-log' // error log
 
-Vue.use(ElementUI)
-Vue.use(VueResource)
-Vue.use(axios)
-Vue.config.productionTip = false
-Vue.prototype.getToken = function (){
-  let item = localStorage.getItem("token");
-  item = JSON.parse(item)
-  let  token_type = item.token_type;
-  let accessToken = item.access_token;
-  console.log(token_type + " " + accessToken)
-  return token_type + " " + accessToken;
+import * as filters from './filters' // global filters
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online! ! !
+ */
+import { mockXHR } from '../mock'
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
 }
-/* eslint-disable no-new */
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
+
+// register global utility filters
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+Vue.config.productionTip = false
+
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  render: h => h(App)
 })
